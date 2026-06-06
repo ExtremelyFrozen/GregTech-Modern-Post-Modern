72da40d2aca543acbca50e49d540217ed8b0b867
QiuYe <qiuye2024github@163.com>
2026-06-06 15:05:12 +0800

    Use block pattern size getters

--- src/main/java/com/gregtechceu/gtceu/api/block/MetaMachineBlock.java
+++ src/main/java/com/gregtechceu/gtceu/api/block/MetaMachineBlock.java
@@ -191,10 +191,8 @@ public class MetaMachineBlock extends Block implements ManagedSyncEntityBlock {
             if (definition instanceof MultiblockMachineDefinition multiblockDefinition) {
                 var pattern = multiblockDefinition.getPatternFactory().get();
                 if (pattern != null) {
-                    var aisleDims = pattern.getDimensions();
-                    assert aisleDims.length == 3;
-                    tooltip.add(Component.translatable("gtpm.multiblock.dimension", aisleDims[0], aisleDims[1],
-                            aisleDims[2]));
+                    tooltip.add(Component.translatable("gtpm.multiblock.dimension", pattern.getFingerLength(),
+                            pattern.getThumbLength(), pattern.getPalmLength()));
                 }
             }
         }
