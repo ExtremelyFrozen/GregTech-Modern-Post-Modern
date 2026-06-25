dbff9b1b736caf23b82a89e24d260b70f7c4fb33
QiuYe <qiuye2024github@163.com>
2026-06-25 12:34:14 +0800

    修复结构谓词限制计数合并

--- src/main/java/com/gregtechceu/gtceu/api/multiblock/structurepredicate/BlockPredicate.java
+++ src/main/java/com/gregtechceu/gtceu/api/multiblock/structurepredicate/BlockPredicate.java
@@ -14,6 +14,7 @@ import net.neoforged.neoforge.common.util.Lazy;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import org.jetbrains.annotations.Unmodifiable;
+import org.jspecify.annotations.Nullable;
 
 import java.util.*;
 
@@ -71,5 +72,24 @@ public final class BlockPredicate implements StructurePredicate {
 
     public List<Block> blocks() {
         return blocks.get();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (BlockPredicate) obj;
+        return Objects.equals(this.blockIds(), that.blockIds());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(blockIds());
+    }
+
+    @Override
+    public String toString() {
+        return "BlockPredicate[" +
+                "blockIds=" + blockIds() + ']';
     }
 }
