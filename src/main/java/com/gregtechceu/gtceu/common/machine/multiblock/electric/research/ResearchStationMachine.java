27af61556942fcb5c203cec59ea7006a4bbf845d
QiuYe <qiuye2024github@163.com>
2026-07-03 00:23:58 +0800

    迁移研究站算力网络

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/research/ResearchStationMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/research/ResearchStationMachine.java
@@ -1,47 +1,35 @@
 package com.gregtechceu.gtceu.common.machine.multiblock.electric.research;
 
 import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
-import com.gregtechceu.gtceu.api.capability.GTCapability;
-import com.gregtechceu.gtceu.api.capability.IOpticalComputationProvider;
-import com.gregtechceu.gtceu.api.capability.IOpticalComputationReceiver;
-import com.gregtechceu.gtceu.api.capability.recipe.CWURecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
-import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
-import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IDisplayUIMachine;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiPart;
 import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockDisplayText;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
-import com.gregtechceu.gtceu.api.machine.trait.RecipeLogic;
-import com.gregtechceu.gtceu.api.recipe.ActionResult;
-import com.gregtechceu.gtceu.api.recipe.GTRecipe;
-import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
+import com.gregtechceu.gtceu.api.machine.trait.NetworkedComputationContainer;
+import com.gregtechceu.gtceu.common.computation.ComputationNetworkManager;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.ObjectHolderMachine;
+import com.gregtechceu.gtceu.common.machine.multiblock.part.OpticalComputationHatchMachine;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.server.level.ServerLevel;
 
 import lombok.Getter;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
 
 public class ResearchStationMachine extends WorkableElectricMultiblockMachine
-                                    implements IOpticalComputationReceiver, IDisplayUIMachine {
+                                    implements IDisplayUIMachine {
 
     @Getter
-    private IOpticalComputationProvider computationProvider;
+    private final NetworkedComputationContainer importComputation;
     @Getter
     private @Nullable ObjectHolderMachine objectHolder;
 
     public ResearchStationMachine(BlockEntityCreationInfo info) {
-        super(info, new ResearchStationRecipeLogic());
-    }
-
-    @Override
-    public ResearchStationRecipeLogic getRecipeLogic() {
-        return (ResearchStationRecipeLogic) super.getRecipeLogic();
+        super(info);
+        this.importComputation = attachTrait(new NetworkedComputationContainer(IO.IN));
     }
 
     @Override
@@ -56,17 +44,9 @@ public class ResearchStationMachine extends WorkableElectricMultiblockMachine
                 }
                 this.objectHolder = holder;
             }
-
-            MetaMachine base = part.self();
-            var provider = getLevel().getCapability(GTCapability.CAPABILITY_COMPUTATION_PROVIDER,
-                    base.getBlockPos(), base.getBlockState(), base, null);
-            if (provider != null) {
-                this.computationProvider = provider;
-            }
         }
 
-        // should never happen, but would rather do this than have an obscure NPE
-        if (computationProvider == null || objectHolder == null) {
+        if (objectHolder == null) {
             invalidateStructure(structureName);
         }
     }
@@ -83,8 +63,6 @@ public class ResearchStationMachine extends WorkableElectricMultiblockMachine
     @Override
     public void invalidateStructure(String structureName) {
         if (DEFAULT_STRUCTURE.equals(structureName)) {
-            computationProvider = null;
-            // recheck the ability to make sure it wasn't the one broken
             for (IMultiPart part : getParts()) {
                 if (part instanceof ObjectHolderMachine holder) {
                     if (holder == objectHolder) {
@@ -102,6 +80,19 @@ public class ResearchStationMachine extends WorkableElectricMultiblockMachine
         return false;
     }
 
+    private int getMaxComputation() {
+        if (!(getLevel() instanceof ServerLevel serverLevel)) {
+            return 0;
+        }
+        for (IMultiPart part : getParts()) {
+            if (part instanceof OpticalComputationHatchMachine opticalMachine) {
+                return ComputationNetworkManager.get(serverLevel)
+                        .getNetWorkAvailableCWUt(opticalMachine.getComputationPort());
+            }
+        }
+        return 0;
+    }
+
     @Override
     public void addDisplayText(List<Component> textList) {
         var workLogic = getWorkLogic();
@@ -112,111 +103,7 @@ public class ResearchStationMachine extends WorkableElectricMultiblockMachine
                 .addEnergyUsageLine(energyContainer)
                 .addEnergyTierLine(tier)
                 .addWorkingStatusLine()
-                // .addComputationUsageExactLine(computationProvider.getMaxCWUt()) // TODO: (Onion)
+                .addComputationUsageLine(getMaxComputation())
                 .addProgressLineOnlyPercent(recipeLogic.getProgressPercent());
-    }
-
-    public static class ResearchStationRecipeLogic extends RecipeLogic {
-
-        public ResearchStationRecipeLogic() {
-            super();
-        }
-
-        @Override
-        public ResearchStationMachine getMachine() {
-            return (ResearchStationMachine) super.getMachine();
-        }
-
-        @Override
-        protected List<Class<?>> validMachineClasses() {
-            return List.of(ResearchStationMachine.class);
-        }
-
-        // skip "can fit" checks, it can always fit
-        @Override
-        protected ActionResult matchRecipe(GTRecipe recipe) {
-            var match = matchRecipeNoOutput(recipe);
-            if (!match.isSuccess()) return match;
-
-            return matchTickRecipeNoOutput(recipe);
-        }
-
-        @Override
-        public boolean checkMatchedRecipeAvailable(GTRecipe match) {
-            var modified = getMachine().fullModifyRecipe(match);
-            if (modified != null) {
-                // What is the point of this
-                if (!modified.inputs.containsKey(CWURecipeCapability.CAP) &&
-                        !modified.tickInputs.containsKey(CWURecipeCapability.CAP)) {
-                    return true;
-                }
-                var recipeMatch = checkRecipe(modified);
-                if (recipeMatch.isSuccess()) {
-                    setupRecipe(modified);
-                } else {
-                    setWaiting(recipeMatch.reason());
-                }
-                if (lastRecipe != null && getStatus() == Status.WORKING) {
-                    lastOriginRecipe = match;
-                    lastFailedMatches = null;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        protected ActionResult matchRecipeNoOutput(GTRecipe recipe) {
-            if (!getMachine().hasCapabilityProxies()) return ActionResult.FAIL_NO_CAPABILITIES;
-            return RecipeHelper.handleRecipe(getMachine(), recipe, IO.IN, recipe.inputs, Collections.emptyMap(), false,
-                    true);
-        }
-
-        protected ActionResult matchTickRecipeNoOutput(GTRecipe recipe) {
-            if (recipe.hasTick()) {
-                if (!getMachine().hasCapabilityProxies()) return ActionResult.FAIL_NO_CAPABILITIES;
-                return RecipeHelper.handleRecipe(getMachine(), recipe, IO.IN, recipe.tickInputs, Collections.emptyMap(),
-                        false, true);
-            }
-            return ActionResult.SUCCESS;
-        }
-
-        // Handle RecipeIO manually
-        @Override
-        protected ActionResult handleRecipeIO(GTRecipe recipe, IO io) {
-            if (io == IO.IN) {
-                // lock the object holder on recipe start
-                ObjectHolderMachine holder = getMachine().getObjectHolder();
-                holder.setLocked(true);
-                return ActionResult.SUCCESS;
-            }
-
-            // "replace" the items in the slots rather than outputting elsewhere
-            // unlock the object holder
-            ObjectHolderMachine holder = getMachine().getObjectHolder();
-            if (lastRecipe == null) {
-                holder.setLocked(false);
-                return ActionResult.SUCCESS;
-            }
-
-            holder.setHeldItem(ItemStack.EMPTY);
-            ItemStack outputItem = ItemStack.EMPTY;
-            var contents = lastRecipe.getOutputContents(ItemRecipeCapability.CAP);
-            if (!contents.isEmpty()) {
-                outputItem = ItemRecipeCapability.CAP.of(contents.get(0).content).getItems()[0];
-            }
-            if (!outputItem.isEmpty()) {
-                holder.setDataItem(outputItem.copy());
-            }
-            holder.setLocked(false);
-            return ActionResult.SUCCESS;
-        }
-
-        @Override
-        protected ActionResult handleTickRecipeIO(GTRecipe recipe, IO io) {
-            if (io != IO.OUT) {
-                return super.handleTickRecipeIO(recipe, io);
-            }
-            return ActionResult.SUCCESS;
-        }
     }
 }
--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/ObjectHolderMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/ObjectHolderMachine.java
@@ -4,8 +4,11 @@ import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.gui.widget.BlockableSlotWidget;
+import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
+import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.handler.RecipeHandlerList;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SaveField;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SyncToClient;
 import com.gregtechceu.gtceu.api.transfer.item.CustomItemStackHandler;
@@ -18,25 +21,27 @@ import com.lowdragmc.lowdraglib.utils.Position;
 
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.crafting.SizedIngredient;
 
-import lombok.Getter;
+import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ObjectHolderMachine extends MultiblockPartMachine {
 
-    // purposefully not exposed to automation or capabilities
     @SaveField
-    private final ObjectHolderHandler heldItems;
-    @Getter
+    private final ObjectInputHandler inputItemHandler;
+    @SaveField
+    private final DataItemHandler dataItemHandler;
     @SaveField
     @SyncToClient
     private boolean isLocked;
 
-    @Getter
-    private NotifiableItemStackHandler handler;
-
     public ObjectHolderMachine(BlockEntityCreationInfo info) {
         super(info);
-        heldItems = attachTrait(new ObjectHolderHandler());
+        inputItemHandler = attachTrait(new ObjectInputHandler());
+        dataItemHandler = attachTrait(new DataItemHandler());
     }
 
     public void setLocked(boolean locked) {
@@ -44,26 +49,30 @@ public class ObjectHolderMachine extends MultiblockPartMachine {
         syncDataHolder.markClientSyncFieldDirty("isLocked");
     }
 
+    public boolean isLocked() {
+        return isLocked;
+    }
+
     public ItemStack getHeldItem(boolean remove) {
-        return getHeldItem(0, remove);
+        return getStack(inputItemHandler, remove);
     }
 
     public void setHeldItem(ItemStack heldItem) {
-        heldItems.setStackInSlot(0, heldItem);
+        inputItemHandler.setStackInSlot(0, heldItem);
     }
 
     public ItemStack getDataItem(boolean remove) {
-        return getHeldItem(1, remove);
+        return getStack(dataItemHandler, remove);
     }
 
     public void setDataItem(ItemStack dataItem) {
-        heldItems.setStackInSlot(1, dataItem);
+        dataItemHandler.setStackInSlot(0, dataItem);
     }
 
-    private ItemStack getHeldItem(int slot, boolean remove) {
-        ItemStack stackInSlot = heldItems.getStackInSlot(slot);
-        if (remove && stackInSlot != ItemStack.EMPTY) {
-            heldItems.setStackInSlot(slot, ItemStack.EMPTY);
+    private ItemStack getStack(NotifiableItemStackHandler handler, boolean remove) {
+        ItemStack stackInSlot = handler.getStackInSlot(0);
+        if (remove && !stackInSlot.isEmpty()) {
+            handler.setStackInSlot(0, ItemStack.EMPTY);
         }
         return stackInSlot;
     }
@@ -72,10 +81,10 @@ public class ObjectHolderMachine extends MultiblockPartMachine {
     public Widget createUIWidget() {
         return new WidgetGroup(new Position(0, 0))
                 .addWidget(new ImageWidget(46, 15, 84, 60, GuiTextures.PROGRESS_BAR_RESEARCH_STATION_BASE))
-                .addWidget(new BlockableSlotWidget(heldItems, 0, 79, 36)
+                .addWidget(new BlockableSlotWidget(inputItemHandler, 0, 79, 36)
                         .setIsBlocked(this::isLocked)
                         .setBackground(GuiTextures.SLOT, GuiTextures.RESEARCH_STATION_OVERLAY))
-                .addWidget(new BlockableSlotWidget(heldItems, 1, 15, 36)
+                .addWidget(new BlockableSlotWidget(dataItemHandler, 0, 15, 36)
                         .setIsBlocked(this::isLocked)
                         .setBackground(GuiTextures.SLOT, GuiTextures.DATA_ORB_OVERLAY));
     }
@@ -91,25 +100,54 @@ public class ObjectHolderMachine extends MultiblockPartMachine {
         }
     }
 
-    private class ObjectHolderHandler extends NotifiableItemStackHandler {
+    @Override
+    public List<RecipeHandlerList> getRecipeHandlers() {
+        return List.of(
+                RecipeHandlerList.of(IO.IN, getPaintingColor(), inputItemHandler),
+                RecipeHandlerList.of(IO.BOTH, getPaintingColor(), dataItemHandler));
+    }
 
-        public ObjectHolderHandler() {
-            super(2, IO.IN, IO.BOTH, size -> new CustomItemStackHandler(size) {
+    @MustBeInvokedByOverriders
+    @Override
+    public void removedFromController(MultiblockControllerMachine controller, String structureName) {
+        super.removedFromController(controller, structureName);
+        setLocked(false);
+    }
 
-                @Override
-                public int getSlotLimit(int slot) {
-                    return 1;
-                }
-            });
+    private boolean isDataItemFacing(@Nullable Direction direction) {
+        return direction == getFrontFacing() || direction == getFrontFacing().getOpposite();
+    }
+
+    private class ObjectInputHandler extends NotifiableItemStackHandler {
+
+        public ObjectInputHandler() {
+            super(1, IO.IN, IO.BOTH, ObjectHolderItemStackHandler::new);
+            capabilityValidator = direction -> direction == null || !isDataItemFacing(direction);
+            setFilter(stack -> stack.isEmpty() || !stack.has(GTDataComponents.DATA_ITEM));
         }
 
-        // only allow a single item, no stack size
         @Override
-        public int getSlotLimit(int slot) {
-            return 1;
+        public boolean isItemValid(int slot, ItemStack stack) {
+            return stack.isEmpty() || !stack.has(GTDataComponents.DATA_ITEM);
+        }
+    }
+
+    private class DataItemHandler extends NotifiableItemStackHandler {
+
+        public DataItemHandler() {
+            super(1, IO.BOTH, IO.BOTH, ObjectHolderItemStackHandler::new);
+            capabilityValidator = direction -> direction == null || isDataItemFacing(direction);
+            setFilter(stack -> stack.isEmpty() || stack.has(GTDataComponents.DATA_ITEM));
         }
 
-        // prevent extracting the item while running
+        @Override
+        public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+            if (!isLocked()) {
+                return super.insertItem(slot, stack, simulate);
+            }
+            return stack;
+        }
+
         @Override
         public ItemStack extractItem(int slot, int amount, boolean simulate) {
             if (!isLocked()) {
@@ -118,17 +156,37 @@ public class ObjectHolderMachine extends MultiblockPartMachine {
             return ItemStack.EMPTY;
         }
 
-        // only allow data items in the second slot
+        @Override
+        public List<SizedIngredient> handleRecipeInner(IO io, GTRecipe recipe, List<SizedIngredient> left,
+                                                       boolean simulate) {
+            if (io == IO.OUT && simulate) {
+                return List.of();
+            }
+            if (io == IO.OUT && !simulate) {
+                setLocked(false);
+            }
+            List<SizedIngredient> result = super.handleRecipeInner(io, recipe, left, simulate);
+            if (result.isEmpty() && !simulate && (io == IO.IN || io == IO.OUT)) {
+                setLocked(io == IO.IN);
+            }
+            return result;
+        }
+
         @Override
         public boolean isItemValid(int slot, ItemStack stack) {
-            if (stack.isEmpty()) {
-                return true;
-            }
+            return stack.isEmpty() || stack.has(GTDataComponents.DATA_ITEM);
+        }
+    }
 
-            boolean isDataItem = stack.has(GTDataComponents.DATA_ITEM);
-            if (slot == 0 && !isDataItem) {
-                return true;
-            } else return slot == 1 && isDataItem;
+    private static class ObjectHolderItemStackHandler extends CustomItemStackHandler {
+
+        public ObjectHolderItemStackHandler(int size) {
+            super(size);
+        }
+
+        @Override
+        public int getSlotLimit(int slot) {
+            return 1;
         }
     }
 }
