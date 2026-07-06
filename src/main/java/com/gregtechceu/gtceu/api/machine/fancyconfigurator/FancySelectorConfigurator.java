57811914cbd61535674ad7464ee97c6696724f53
QiuYe <qiuye2024github@163.com>
2026-07-07 02:41:56 +0800

    移除枚举选择器LDLib1控件

--- src/main/java/com/gregtechceu/gtceu/api/machine/fancyconfigurator/FancySelectorConfigurator.java
+++ src/main/java/com/gregtechceu/gtceu/api/machine/fancyconfigurator/FancySelectorConfigurator.java
@@ -1,10 +1,10 @@
 package com.gregtechceu.gtceu.api.machine.fancyconfigurator;
 
-import com.gregtechceu.gtceu.api.gui.fancy.IFancyConfiguratorButton;
+import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.gui.SelectableEnum;
-import com.gregtechceu.gtceu.api.gui.widget.EnumSelectorWidget;
-
+import com.gregtechceu.gtceu.api.gui.fancy.IFancyConfiguratorButton;
 import com.gregtechceu.gtceu.api.gui.texture.IGuiTexture;
+
 import com.lowdragmc.lowdraglib2.gui.util.ClickData;
 
 import net.minecraft.network.chat.Component;
@@ -14,42 +14,52 @@ import lombok.experimental.Accessors;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class FancySelectorConfigurator<T extends Enum<T> & SelectableEnum>
                                       implements IFancyConfiguratorButton {
 
-    private final EnumSelectorWidget<T> widget;
+    private final List<T> values;
+    private final Consumer<T> onChanged;
+    private int selected;
 
     @Setter
     @Accessors(chain = true)
     private Function<T, List<Component>> tooltip = t -> Collections.singletonList(Component.empty());
 
     public FancySelectorConfigurator(T[] values, T initialValue, Consumer<T> onChanged) {
-        this.widget = new EnumSelectorWidget<>(0, 0, 20, 20, values, initialValue, onChanged);
+        this.values = List.of(values);
+        this.onChanged = onChanged;
+        setSelected(initialValue);
+        onChanged.accept(getCurrentValue());
     }
 
     @Override
     public IGuiTexture getIcon() {
-        return widget.getTexture(widget.selected);
+        return GuiTextures.group(GuiTextures.VANILLA_BUTTON, getCurrentValue().getIcon());
     }
 
     @Override
     public List<Component> getTooltips() {
-        return this.tooltip.apply(widget.getCurrentValue());
+        return this.tooltip.apply(getCurrentValue());
     }
 
     @Override
     public void onClick(ClickData clickData) {
-        ++widget.selected;
-        if (widget.selected >= widget.values.size()) {
-            widget.selected = 0;
-        }
+        selected = (selected + 1) % values.size();
+        onChanged.accept(getCurrentValue());
+    }
 
-        widget.buttonWidget.setIndex(widget.selected);
-        if (widget.onChanged != null) {
-            widget.onChanged.accept(widget.getCurrentValue());
+    private T getCurrentValue() {
+        return values.get(selected);
+    }
+
+    private void setSelected(T value) {
+        selected = values.indexOf(value);
+        if (selected == -1) {
+            throw new NoSuchElementException(value + " is not a possible value for this selector.");
         }
     }
 }
