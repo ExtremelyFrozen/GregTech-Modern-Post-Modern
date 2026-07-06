54f2cdde8a15f6490cb913496ddcb36f3cb6e0cf
QiuYe <qiuye2024github@163.com>
2026-07-06 22:57:28 +0800

    迁移存储覆盖板LDLib2配置界面

--- src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
+++ src/main/java/com/gregtechceu/gtceu/common/cover/StorageCover.java
@@ -3,31 +3,31 @@ package com.gregtechceu.gtceu.common.cover;
 import com.gregtechceu.gtceu.api.capability.ICoverable;
 import com.gregtechceu.gtceu.api.cover.CoverBehavior;
 import com.gregtechceu.gtceu.api.cover.CoverDefinition;
-import com.gregtechceu.gtceu.api.cover.IUICover;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
-import com.gregtechceu.gtceu.api.gui.fancy.IFancyConfigurator;
-import com.gregtechceu.gtceu.api.gui.texture.IGuiTexture;
-import com.gregtechceu.gtceu.api.gui.widget.SlotWidget;
+import com.gregtechceu.gtceu.api.gui.UITemplate;
+import com.gregtechceu.gtceu.api.gui.element.GTItemSlotElement;
+import com.gregtechceu.gtceu.api.gui.element.GTLabelElement;
+import com.gregtechceu.gtceu.api.gui.factory.LDLib2CoverUIProvider;
+import com.gregtechceu.gtceu.api.gui.factory.UICoverHolder;
 import com.gregtechceu.gtceu.api.machine.MachineCoverContainer;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SaveField;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SyncToClient;
 import com.gregtechceu.gtceu.api.transfer.item.CustomItemStackHandler;
 
-import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
-import com.lowdragmc.lowdraglib.gui.widget.Widget;
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
-import com.lowdragmc.lowdraglib2.utils.LocalizationUtils;
+import com.lowdragmc.lowdraglib2.gui.ui.UI;
+import com.lowdragmc.lowdraglib2.gui.ui.UIElement;
+import com.lowdragmc.lowdraglib2.gui.ui.data.Horizontal;
+import com.lowdragmc.lowdraglib2.gui.ui.data.Vertical;
 
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class StorageCover extends CoverBehavior implements IUICover {
+public class StorageCover extends CoverBehavior implements LDLib2CoverUIProvider {
 
     @SaveField
     @SyncToClient
@@ -68,49 +68,44 @@ public class StorageCover extends CoverBehavior implements IUICover {
         return super.canAttach();
     }
 
-    @Override
-    public Widget createUIWidget() {
-        final var group = new WidgetGroup(0, 0, 126, 87);
-
-        group.addWidget(new LabelWidget(10, 5, LocalizationUtils.format(getUITitle())));
-
-        for (int slot = 0; slot < SIZE; slot++) {
-            group.addWidget(new SlotWidget(inventory, slot, 7 + (slot % 6) * 18, 21 + (slot / 6) * 18));
-        }
-
-        return group;
-    }
-
     private String getUITitle() {
         return "cover.storage.title";
     }
 
     @Override
-    public @Nullable IFancyConfigurator getConfigurator() {
-        return new StorageCoverConfigurator();
+    public boolean canCreateLDLib2UI(Player player, UICoverHolder holder) {
+        return holder.getCover() == this;
     }
 
-    private class StorageCoverConfigurator implements IFancyConfigurator {
+    @Override
+    public UI createLDLib2UI(Player player, UICoverHolder holder) {
+        UIElement root = new UIElement();
+        UITemplate.setLDLib2Bounds(root, 0, 0, 176, 87 + 82);
+        root.style(style -> style.backgroundTexture(GuiTextures.BACKGROUND));
 
-        @Override
-        public Component getTitle() {
-            return Component.translatable("cover.storage.title");
+        int centeredGroupX = (176 - 126) / 2;
+        root.addChild(createLDLib2Label(centeredGroupX));
+        for (int slot = 0; slot < SIZE; slot++) {
+            root.addChild(createLDLib2StorageSlot(slot, centeredGroupX));
         }
+        root.addChild(UITemplate.bindPlayerInventoryLDLib2(player.getInventory(), GuiTextures.SLOT, 7, 87, true));
+        return UI.of(root);
+    }
 
-        @Override
-        public IGuiTexture getIcon() {
-            return GuiTextures.STORAGE_ICON;
-        }
+    private GTLabelElement createLDLib2Label(int groupX) {
+        GTLabelElement label = new GTLabelElement(groupX + 10, 5, 116, 10, getUITitle(), true);
+        label.textStyle(style -> style
+                .textColor(0x404040)
+                .textShadow(false)
+                .textAlignHorizontal(Horizontal.LEFT)
+                .textAlignVertical(Vertical.CENTER));
+        return label;
+    }
 
-        @Override
-        public Widget createConfigurator() {
-            final var group = new WidgetGroup(0, 0, 126, 87);
-
-            for (int slot = 0; slot < SIZE; slot++) {
-                group.addWidget(new SlotWidget(inventory, slot, 7 + (slot % 6) * 18, 21 + (slot / 6) * 18));
-            }
-
-            return group;
-        }
+    private GTItemSlotElement createLDLib2StorageSlot(int slot, int groupX) {
+        GTItemSlotElement element = new GTItemSlotElement(inventory, slot);
+        UITemplate.setLDLib2Bounds(element, groupX + 7 + (slot % 6) * 18, 21 + (slot / 6) * 18, 18, 18);
+        element.setBackgroundTexture(GuiTextures.SLOT);
+        return element;
     }
 }
