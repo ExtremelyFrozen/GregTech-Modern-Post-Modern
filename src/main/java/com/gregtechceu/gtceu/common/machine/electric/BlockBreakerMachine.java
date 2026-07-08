b04bffddf3e362f7ea486c53e50e5289f0dcd6dd
QiuYe <qiuye2024github@163.com>
2026-07-08 18:25:51 +0800

    覆盖LDLib2方块破坏机UI

--- src/main/java/com/gregtechceu/gtceu/common/machine/electric/BlockBreakerMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/electric/BlockBreakerMachine.java
@@ -6,13 +6,12 @@ import com.gregtechceu.gtceu.api.capability.GTCapabilityHelper;
 import com.gregtechceu.gtceu.api.capability.IControllable;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
-import com.gregtechceu.gtceu.api.gui.WidgetUtils;
-import com.gregtechceu.gtceu.api.gui.editor.EditableMachineUI;
-import com.gregtechceu.gtceu.api.gui.editor.EditableUI;
-import com.gregtechceu.gtceu.api.gui.widget.SlotWidget;
+import com.gregtechceu.gtceu.api.gui.UITemplate;
+import com.gregtechceu.gtceu.api.gui.element.GTItemSlotElement;
+import com.gregtechceu.gtceu.api.gui.fancy.LDLib2FancyMachineUIElement;
 import com.gregtechceu.gtceu.api.machine.TickableSubscription;
 import com.gregtechceu.gtceu.api.machine.TieredEnergyMachine;
-import com.gregtechceu.gtceu.api.machine.feature.IFancyUIMachine;
+import com.gregtechceu.gtceu.api.machine.feature.LDLib2FancyUIMachine;
 import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SaveField;
 import com.gregtechceu.gtceu.api.sync_system.annotations.SyncToClient;
@@ -22,15 +21,12 @@ import com.gregtechceu.gtceu.config.ConfigHolder;
 import com.gregtechceu.gtceu.data.lang.LangHandler;
 import com.gregtechceu.gtceu.utils.ISubscription;
 
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
-import com.lowdragmc.lowdraglib.utils.Position;
+import com.lowdragmc.lowdraglib2.gui.ui.UIElement;
 
-import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
@@ -38,7 +34,6 @@ import lombok.Getter;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.function.BiFunction;
 
 /**
  * @author h3tr
@@ -46,7 +41,18 @@ import java.util.function.BiFunction;
  * @implNote BlockBreakerMachine
  */
 public class BlockBreakerMachine extends TieredEnergyMachine
-                                 implements IFancyUIMachine, IControllable {
+                                 implements LDLib2FancyUIMachine, IControllable {
+
+    private static final int SLOT_SIZE = 18;
+    private static final int TEMPLATE_PADDING = 8;
+    private static final int TEMPLATE_SLOT_OFFSET = 4;
+    private static final int ENERGY_BAR_WIDTH = 18;
+    private static final int ENERGY_BAR_HEIGHT = 60;
+    private static final int ENERGY_GROUP_X = 3;
+    private static final int ENERGY_GROUP_CHARGER_HEIGHT = 20;
+    private static final int ENERGY_GROUP_CHARGER_GAP = 1;
+    private static final int PAGE_MIN_WIDTH = 172;
+    private static final int PAGE_ENERGY_TEMPLATE_GAP = 4;
 
     @SaveField
     protected final NotifiableItemStackHandler cache;
@@ -139,7 +145,7 @@ public class BlockBreakerMachine extends TieredEnergyMachine
     }
 
     @Override
-    public void onNeighborChanged(net.minecraft.world.level.block.Block block, BlockPos fromPos, boolean isMoving) {
+    public void onNeighborChanged(Block block, BlockPos fromPos, boolean isMoving) {
         super.onNeighborChanged(block, fromPos, isMoving);
         updateBreakerSubscription();
     }
@@ -174,9 +180,9 @@ public class BlockBreakerMachine extends TieredEnergyMachine
                         var remainder = tryFillCache(drop);
                         if (!remainder.isEmpty()) {
                             if (autoOutput.getItemOutputDirection() == null) {
-                                net.minecraft.world.level.block.Block.popResource(getLevel(), getBlockPos(), remainder);
+                                Block.popResource(getLevel(), getBlockPos(), remainder);
                             } else {
-                                net.minecraft.world.level.block.Block.popResource(getLevel(),
+                                Block.popResource(getLevel(),
                                         getBlockPos().relative(autoOutput.getItemOutputDirection()),
                                         remainder);
                             }
@@ -216,7 +222,7 @@ public class BlockBreakerMachine extends TieredEnergyMachine
     }
 
     private List<ItemStack> tryDestroyBlockAndGetDrops(BlockPos pos) {
-        List<ItemStack> drops = net.minecraft.world.level.block.Block.getDrops(getLevel().getBlockState(pos),
+        List<ItemStack> drops = Block.getDrops(getLevel().getBlockState(pos),
                 (ServerLevel) getLevel(), pos, null, null, ItemStack.EMPTY);
         getLevel().destroyBlock(pos, false);
         return drops;
@@ -268,77 +274,98 @@ public class BlockBreakerMachine extends TieredEnergyMachine
     //////////////////////////////////////
     // ********** GUI ***********//
     //////////////////////////////////////
-    public static BiFunction<ResourceLocation, Integer, EditableMachineUI> EDITABLE_UI_CREATOR = Util
-            .memoize((path, inventorySize) -> new EditableMachineUI("misc", path, () -> {
-                var template = createTemplate(inventorySize).createDefault();
-                var energyBar = createEnergyBar().createDefault();
-                var batterySlot = createBatterySlot().createDefault();
-                var energyGroup = new WidgetGroup(0, 0, energyBar.getSize().width, energyBar.getSize().height + 20);
-                batterySlot.setSelfPosition(
-                        new Position((energyBar.getSize().width - 18) / 2, energyBar.getSize().height + 1));
-                energyGroup.addWidget(energyBar);
-                energyGroup.addWidget(batterySlot);
-                var group = new WidgetGroup(0, 0,
-                        Math.max(energyGroup.getSize().width + template.getSize().width + 4 + 8, 172),
-                        Math.max(template.getSize().height + 8, energyGroup.getSize().height + 8));
-                var size = group.getSize();
-                energyGroup.setSelfPosition(new Position(3, (size.height - energyGroup.getSize().height) / 2));
+    @Override
+    public UIElement createLDLib2MainPage(LDLib2FancyMachineUIElement shell) {
+        UIElement root = UITemplate.setLDLib2Bounds(new UIElement(), 0, 0,
+                getLDLib2PageWidth(), getLDLib2PageHeight());
 
-                template.setSelfPosition(new Position(
-                        (size.width - 4 - template.getSize().width) / 2 + 4,
-                        (size.height - template.getSize().height) / 2));
+        UIElement energyGroup = UITemplate.setLDLib2Bounds(new UIElement(), ENERGY_GROUP_X, getLDLib2EnergyGroupY(),
+                getLDLib2EnergyGroupWidth(), getLDLib2EnergyGroupHeight());
+        energyGroup.addChild(createLDLib2EnergyBar());
+        energyGroup.addChild(createLDLib2ChargerSlot(getLDLib2ChargerSlotX(),
+                ENERGY_BAR_HEIGHT + ENERGY_GROUP_CHARGER_GAP));
+        root.addChild(energyGroup);
 
-                group.addWidget(energyGroup);
-                group.addWidget(template);
-                return group;
-            }, (template, machine) -> {
-                if (machine instanceof BlockBreakerMachine blockBreakerMachine) {
-                    createTemplate(inventorySize).setupUI(template, blockBreakerMachine);
-                    createEnergyBar().setupUI(template, blockBreakerMachine);
-                    createBatterySlot().setupUI(template, blockBreakerMachine);
-                }
-            }));
-
-    protected static EditableUI<SlotWidget, BlockBreakerMachine> createBatterySlot() {
-        return new EditableUI<>("battery_slot", SlotWidget.class, () -> {
-            var slotWidget = new SlotWidget();
-            slotWidget.setBackground(GuiTextures.SLOT, GuiTextures.CHARGER_OVERLAY);
-            return slotWidget;
-        }, (slotWidget, machine) -> {
-            slotWidget.setHandlerSlot(machine.chargerInventory, 0);
-            slotWidget.setCanPutItems(true);
-            slotWidget.setCanTakeItems(true);
-            slotWidget.setHoverTooltips(LangHandler.getMultiLang("gtpm.gui.charger_slot.tooltip",
-                    GTValues.VNF[machine.getTier()], GTValues.VNF[machine.getTier()]).toArray(new MutableComponent[0]));
-        });
+        UIElement template = UITemplate.setLDLib2Bounds(new UIElement(), getLDLib2CacheTemplateX(),
+                getLDLib2CacheTemplateY(), getLDLib2CacheTemplateWidth(), getLDLib2CacheTemplateHeight());
+        template.style(style -> style.backgroundTexture(GuiTextures.BACKGROUND_INVERSE));
+        int rowSize = getLDLib2CacheRowSize();
+        for (int y = 0; y < rowSize; y++) {
+            for (int x = 0; x < rowSize; x++) {
+                int index = y * rowSize + x;
+                template.addChild(createLDLib2CacheSlot(index, TEMPLATE_SLOT_OFFSET + x * SLOT_SIZE,
+                        TEMPLATE_SLOT_OFFSET + y * SLOT_SIZE));
+            }
+        }
+        root.addChild(template);
+        return root;
     }
 
-    protected static EditableUI<WidgetGroup, BlockBreakerMachine> createTemplate(int inventorySize) {
-        return new EditableUI<>("functional_container", WidgetGroup.class, () -> {
-            int rowSize = (int) Math.sqrt(inventorySize);
-            WidgetGroup main = new WidgetGroup(0, 0, rowSize * 18 + 8, rowSize * 18 + 8);
-            for (int y = 0; y < rowSize; y++) {
-                for (int x = 0; x < rowSize; x++) {
-                    int index = y * rowSize + x;
-                    SlotWidget slotWidget = new SlotWidget();
-                    slotWidget.initTemplate();
-                    slotWidget.setSelfPosition(new Position(4 + x * 18, 4 + y * 18));
-                    slotWidget.setBackground(GuiTextures.SLOT);
-                    slotWidget.setId("slot_" + index);
-                    main.addWidget(slotWidget);
-                }
-            }
-            main.setBackground(GuiTextures.BACKGROUND_INVERSE);
-            return main;
-        }, (group, machine) -> {
-            WidgetUtils.widgetByIdForEach(group, "^slot_[0-9]+$", SlotWidget.class, slot -> {
-                var index = WidgetUtils.widgetIdIndex(slot);
-                if (index >= 0 && index < machine.cache.getSlots()) {
-                    slot.setHandlerSlot(machine.cache, index);
-                    slot.setCanTakeItems(true);
-                    slot.setCanPutItems(false);
-                }
-            });
-        });
+    @Override
+    public int getLDLib2PageWidth() {
+        return Math.max(getLDLib2EnergyGroupWidth() + getLDLib2CacheTemplateWidth() +
+                PAGE_ENERGY_TEMPLATE_GAP + TEMPLATE_PADDING, PAGE_MIN_WIDTH);
+    }
+
+    @Override
+    public int getLDLib2PageHeight() {
+        return Math.max(getLDLib2CacheTemplateHeight() + TEMPLATE_PADDING,
+                getLDLib2EnergyGroupHeight() + TEMPLATE_PADDING);
+    }
+
+    private GTItemSlotElement createLDLib2CacheSlot(int index, int x, int y) {
+        GTItemSlotElement slot = new GTItemSlotElement(cache, index)
+                .setCanTakeItems(true)
+                .setCanPutItems(false)
+                .setBackgroundTexture(GuiTextures.SLOT);
+        return UITemplate.setLDLib2Bounds(slot, x, y, SLOT_SIZE, SLOT_SIZE);
+    }
+
+    private GTItemSlotElement createLDLib2ChargerSlot(int x, int y) {
+        GTItemSlotElement slot = new GTItemSlotElement(chargerInventory, 0)
+                .setCanPutItems(true)
+                .setCanTakeItems(true)
+                .setBackgroundTexture(GuiTextures.group(GuiTextures.SLOT, GuiTextures.CHARGER_OVERLAY))
+                .setOnAddedTooltips((slotElement, tooltips) -> tooltips.addAll(
+                        LangHandler.getMultiLang("gtpm.gui.charger_slot.tooltip",
+                                GTValues.VNF[getTier()], GTValues.VNF[getTier()])));
+        return UITemplate.setLDLib2Bounds(slot, x, y, SLOT_SIZE, SLOT_SIZE);
+    }
+
+    private int getLDLib2EnergyGroupWidth() {
+        return ENERGY_BAR_WIDTH;
+    }
+
+    private int getLDLib2EnergyGroupHeight() {
+        return ENERGY_BAR_HEIGHT + ENERGY_GROUP_CHARGER_HEIGHT;
+    }
+
+    private int getLDLib2EnergyGroupY() {
+        return (getLDLib2PageHeight() - getLDLib2EnergyGroupHeight()) / 2;
+    }
+
+    private int getLDLib2ChargerSlotX() {
+        return (ENERGY_BAR_WIDTH - SLOT_SIZE) / 2;
+    }
+
+    private int getLDLib2CacheTemplateX() {
+        return (getLDLib2PageWidth() - PAGE_ENERGY_TEMPLATE_GAP - getLDLib2CacheTemplateWidth()) / 2 +
+                PAGE_ENERGY_TEMPLATE_GAP;
+    }
+
+    private int getLDLib2CacheTemplateY() {
+        return (getLDLib2PageHeight() - getLDLib2CacheTemplateHeight()) / 2;
+    }
+
+    private int getLDLib2CacheTemplateWidth() {
+        return getLDLib2CacheRowSize() * SLOT_SIZE + TEMPLATE_PADDING;
+    }
+
+    private int getLDLib2CacheTemplateHeight() {
+        return getLDLib2CacheRowSize() * SLOT_SIZE + TEMPLATE_PADDING;
+    }
+
+    private int getLDLib2CacheRowSize() {
+        return (int) Math.sqrt(inventorySize);
     }
 }
