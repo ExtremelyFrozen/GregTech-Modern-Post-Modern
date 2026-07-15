d8cb26cda1327a77f56a962f056e879f00716201
QiuYe <qiuye2024github@163.com>
2026-07-15 18:26:27 +0800

    支持光学数据仓LDLib2多方块预览页面

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/OpticalDataHatchMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/part/OpticalDataHatchMachine.java
@@ -4,7 +4,11 @@ import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
 import com.gregtechceu.gtceu.api.capability.GTCapability;
 import com.gregtechceu.gtceu.api.capability.IDataAccessMachine;
 import com.gregtechceu.gtceu.api.capability.IOpticalDataAccessHatch;
+import com.gregtechceu.gtceu.api.gui.factory.MachineUIHolder;
+import com.gregtechceu.gtceu.api.gui.fancy.LDLib2FancyPreviewPage;
+import com.gregtechceu.gtceu.api.gui.fancy.LDLib2FancyUIProvider;
 import com.gregtechceu.gtceu.api.machine.feature.IRecipeLogicMachine;
+import com.gregtechceu.gtceu.api.machine.feature.multiblock.LDLib2FancyPartUIProvider;
 import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.part.MultiblockPartMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
@@ -17,7 +21,8 @@ import net.minecraft.world.phys.BlockHitResult;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-public class OpticalDataHatchMachine extends MultiblockPartMachine implements IOpticalDataAccessHatch {
+public class OpticalDataHatchMachine extends MultiblockPartMachine
+                                     implements IOpticalDataAccessHatch, LDLib2FancyPartUIProvider {
 
     @Getter
     private final boolean isTransmitter;
@@ -66,6 +71,12 @@ public class OpticalDataHatchMachine extends MultiblockPartMachine implements IO
     @Override
     public boolean shouldOpenUI(Player player, InteractionHand hand, BlockHitResult hit) {
         return false;
+    }
+
+    /** Creates the holder-scoped default preview used by a surrounding multiblock controller. */
+    @Override
+    public LDLib2FancyUIProvider createLDLib2FancyPage(Player player, MachineUIHolder holder) {
+        return new LDLib2FancyPreviewPage(this, player, holder, null);
     }
 
     @Override
