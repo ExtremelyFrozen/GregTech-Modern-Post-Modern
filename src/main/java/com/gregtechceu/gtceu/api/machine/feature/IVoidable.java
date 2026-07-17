0b7b9520938e75b6e109ca2c8687b83cfdec7497
QiuYe <qiuye2024github@163.com>
2026-07-17 21:09:17 +0800

    移除旧Fancy配置器

--- src/main/java/com/gregtechceu/gtceu/api/machine/feature/IVoidable.java
+++ src/main/java/com/gregtechceu/gtceu/api/machine/feature/IVoidable.java
@@ -5,18 +5,14 @@ import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.RecipeCapability;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.gui.SelectableEnum;
-import com.gregtechceu.gtceu.api.gui.fancy.ConfiguratorPanel;
 import com.gregtechceu.gtceu.api.gui.texture.IGuiTexture;
-import com.gregtechceu.gtceu.api.machine.fancyconfigurator.FancySelectorConfigurator;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.util.StringRepresentable;
 
 import it.unimi.dsi.fastutil.objects.Reference2IntMap;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.function.Predicate;
 
 public interface IVoidable extends IMachineFeature {
@@ -34,14 +30,6 @@ public interface IVoidable extends IMachineFeature {
 
     default VoidingMode getVoidingMode() {
         return VoidingMode.VOID_NONE;
-    }
-
-    static void attachConfigurators(ConfiguratorPanel configuratorPanel, IVoidable controller) {
-        configuratorPanel
-                .attachConfigurators(new FancySelectorConfigurator<>(VoidingMode.VALUES, controller.getVoidingMode(),
-                        controller::setVoidingMode)
-                        .setTooltip(m -> List.of(Component.translatable("gtpm.gui.multiblock.voiding_mode"),
-                                Component.translatable(m.localeName))));
     }
 
     enum VoidingMode implements StringRepresentable, SelectableEnum {
