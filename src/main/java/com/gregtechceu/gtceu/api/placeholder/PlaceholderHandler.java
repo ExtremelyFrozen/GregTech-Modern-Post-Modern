cf8008521b8ac1f10ea8136d19f16c58f23060bc
QiuYe <qiuye2024github@163.com>
2026-07-17 20:42:02 +0800

    移除旧占位符参考界面

--- src/main/java/com/gregtechceu/gtceu/api/placeholder/PlaceholderHandler.java
+++ src/main/java/com/gregtechceu/gtceu/api/placeholder/PlaceholderHandler.java
@@ -1,20 +1,12 @@
 package com.gregtechceu.gtceu.api.placeholder;
 
 import com.gregtechceu.gtceu.GTCEu;
-import com.gregtechceu.gtceu.api.gui.texture.TextTexture;
 import com.gregtechceu.gtceu.api.placeholder.exceptions.PlaceholderException;
 import com.gregtechceu.gtceu.api.placeholder.exceptions.UnclosedBracketException;
 import com.gregtechceu.gtceu.api.placeholder.exceptions.UnexpectedBracketException;
 import com.gregtechceu.gtceu.api.placeholder.exceptions.UnknownPlaceholderException;
 import com.gregtechceu.gtceu.client.renderer.monitor.IMonitorRenderer;
-import com.gregtechceu.gtceu.data.lang.LangHandler;
-import com.gregtechceu.gtceu.utils.GTStringUtils;
 import com.gregtechceu.gtceu.utils.GTUtil;
-
-import com.lowdragmc.lowdraglib.gui.widget.DraggableScrollableWidgetGroup;
-import com.lowdragmc.lowdraglib.gui.widget.TextTextureWidget;
-import com.lowdragmc.lowdraglib.gui.widget.Widget;
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.MethodsReturnNonnullByDefault;
@@ -26,7 +18,6 @@ import net.neoforged.api.distmarker.OnlyIn;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -189,35 +180,5 @@ public class PlaceholderHandler {
 
     public static Set<String> getAllPlaceholderNames() {
         return placeholders.keySet();
-    }
-
-    public static Widget getPlaceholderHandlerUI(String filter) {
-        DraggableScrollableWidgetGroup placeholderReference = new DraggableScrollableWidgetGroup(280, 15, 100, 200);
-        Consumer<String> onSearch = (newSearch) -> {
-            placeholderReference.clearAllWidgets();
-            int y = 2;
-            ArrayList<String> placeholders = new ArrayList<>(getAllPlaceholderNames());
-            placeholders.removeIf(s -> s == null || !s.contains(newSearch));
-            placeholders.sort(String::compareTo);
-            for (String placeholder : placeholders) {
-                TextTextureWidget placeholderName = new TextTextureWidget(0, y, 80, 15, placeholder);
-                placeholderName.getTextTexture().type = TextTexture.TextType.LEFT;
-                placeholderName.setHoverTooltips(GTStringUtils
-                        .toImmutable(LangHandler.getSingleOrMultiLang("gtpm.placeholder_info." + placeholder)));
-                placeholderReference.addWidget(placeholderName);
-                y += 15;
-            }
-        };
-        onSearch.accept(filter);
-        TextTextureWidget placeholderReferenceLabel = new TextTextureWidget(
-                280, 0,
-                160, 15,
-                GTStringUtils.componentsToString(
-                        LangHandler.getMultiLang("gtpm.gui.computer_monitor_cover.placeholder_reference")));
-        placeholderReferenceLabel.getTextTexture().type = TextTexture.TextType.LEFT;
-        WidgetGroup out = new WidgetGroup();
-        out.addWidget(placeholderReferenceLabel);
-        out.addWidget(placeholderReference);
-        return out;
     }
 }
